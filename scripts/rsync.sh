0b4bdb769cab32277c434d9f508b62df901ad51f
Valentin Saugnier <valentin.s.10@gmail.com>
2023-06-14 11:24:06 +0200

    Rsync with daemon != ssh

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,1 +1,2 @@
-rsync -avz -e "ssh -i /home/valentin/.ssh/id_rsa" /home/valentin/docker/storage valentin@192.168.1.254:/home/valetin/Data/cameras/opi
+rsync -rvz --size-only /home/valentin/docker/storage/ rsync://192.168.1.254:873/opi
+#rsync -avz --no-perms --no-owner --no-group no-times --size-only /home/valentin/docker/storage/ rsync://192.168.1.254:873/opi
